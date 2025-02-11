75d550f91d01a38ae84e725b9f40b2cbd48b7886
Andy Freidenfelds <afreidenfelds@gmail.com>
2025-02-10 20:56:32 -0600

    refactor: update Prisma client integration with PostgreSQL adapter and enhance TypeScript configuration

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,11 @@
-import { PrismaClient } from "@prisma-client";
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import { DATABASE_URL } from "astro:env/server";
 
-const client = new PrismaClient();
-export default client;
+const connectionString = `${DATABASE_URL}`;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+export default prisma;
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { PUBLIC_SB_PROJECT_ID, SB_DB_SR_KEY } from "astro:env/server";
 
-console.log("ANDY", PUBLIC_SB_PROJECT_ID, SB_DB_SR_KEY);
-
 const client = createClient(
   `https://${PUBLIC_SB_PROJECT_ID}.supabase.co`,
   SB_DB_SR_KEY
